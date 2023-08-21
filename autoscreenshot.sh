a990533f54f72777ae5b855e5b2b59feeb397136
dngst <dngst@protonmail.com>
2023-08-21 13:19:23 +0300

    Remove file

--- autoscreenshot.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/bash
-
-xfce4-screenshooter --fullscreen --save ~/Pictures/"Screenshot $(date -d "today" +"%Y-%m-%d %H:%M").png"
--- connectivity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -q --spider http://google.com
-
-if [ $? -eq 0 ]; then
-    notify-send -u critical "Network" "Online"
-else
-    notify-send -u critical "Network" "Offline"
-fi
