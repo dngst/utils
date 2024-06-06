9023f49bc63205376fa947afe7fcbabe4e3d91e7
dngst <dngst@protonmail.com>
2024-06-06 15:41:34 +0300

    Generate file name from time stamp and play sound feedback

--- scrot/areashot.sh
+++ scrot/areashot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-scrot -s -q 100 ~/Pictures/
+scrot -s -q 100 ~/Pictures/Screenshot_$(date +%Y-%m-%d_%H-%M-%S).png
+paplay /usr/share/sounds/freedesktop/stereo/camera-shutter.oga
 
--- scrot/screenshot.sh
+++ scrot/screenshot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-scrot -q 100 ~/Pictures/
+scrot -q 100 ~/Pictures/Screenshot_$(date +%Y-%m-%d_%H-%M-%S).png
+paplay /usr/share/sounds/freedesktop/stereo/camera-shutter.oga
 
